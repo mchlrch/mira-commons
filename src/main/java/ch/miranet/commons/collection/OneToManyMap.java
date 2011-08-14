22ffffa5801b54faa6db752308e99f2d5e4ee418
Michael Rauch <mira@miranet.ch>
2011-08-14 21:08:32 +0200

    add missing headers

--- src/main/java/ch/miranet/commons/collection/OneToManyMap.java
+++ src/main/java/ch/miranet/commons/collection/OneToManyMap.java
@@ -1,3 +1,22 @@
+/*********************************************************************
+  This file is part of the 'mira-commons' library,
+  see <http://www.miranet.ch/projects/mira-commons>
+
+  Copyright (C) 2011 Michael Rauch
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+      
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License. 
+ *********************************************************************/
+
 package ch.miranet.commons.collection;
 
 import java.util.Collection;
@@ -6,12 +25,8 @@ import java.util.Set;
 
 public interface OneToManyMap<K, V> extends Map<K, Set<V>> {
 
-	// PEND: unklar ob overloading die bessere wahl ist 
 	boolean add(K key, V value);
 	boolean add(K key, Collection<? extends V> vals);
-//	boolean addOne(K key, V value);
-//	boolean addMany(K key, Collection<? extends V> vals);
-
 	boolean addAll(OneToManyMap<? extends K, ? extends V> map);
 
 }
